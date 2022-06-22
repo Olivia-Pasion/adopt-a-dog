de95b069563f170e33fe66ecc605d2b58cf3796a
olivia pasion <oap9820@gmail.com>
2022-06-21 17:29:59 -0700

    completed

--- services/adopt-service.js
+++ services/adopt-service.js
@@ -5,6 +5,9 @@ const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 export async function getDogs() {
     // *** from the dogs table, select all items
     // but you only need id, name, and breed columns
+    const response = await client
+        .from('dogs')
+        .select('name', 'age', 'breed');
 
 
     // and return the response
@@ -14,6 +17,11 @@ export async function getDogs() {
 export async function getDog(id) {
     // *** from the dogs table, select a >>single<< dog who has the >>matching id<<
     // return all columns
+    const response = await client
+        .from('dogs')
+        .select()
+        .match({ id })
+        .single();
 
     // and return the response
     return response.data;
